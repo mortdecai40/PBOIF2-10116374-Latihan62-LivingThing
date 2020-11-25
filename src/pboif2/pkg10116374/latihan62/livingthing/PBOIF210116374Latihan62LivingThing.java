404f4e4b5bccd35120e9ca8d7f214d2b5ea5e576
mortdecai40 <rizkinabil.999@gmail.com>
2020-11-25 15:01:06 +0700

    Latihan 62

---
+++ src/pboif2/pkg10116374/latihan62/livingthing/PBOIF210116374Latihan62LivingThing.java
@@ -0,0 +1,24 @@
+package pboif2.pkg10116374.latihan62.livingthing;
+
+/**
+ * @author Acromyrmex
+ * NAMA                 : TEUKU RIZKI NABIL
+ * KELAS                : PBO2
+ * NIM                  : 10116374
+ * Deskripsi Program    : Program untuk menampilkan 3 method yang ada
+ *
+ */
+public class PBOIF210116374Latihan62LivingThing {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Human human= new Human();
+        human.walk("Teuku Rizki Nabil");
+        human.breath("Teuku Rizki Nabil");
+        human.eat("Teuku Rizki Nabil");
+    }
+    
+}
